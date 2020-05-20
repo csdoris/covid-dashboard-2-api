716818ad61554499c3436a4ae1d3780682a9587f
csdoris <cs.doris@gmail.com>
2020-05-20 12:19:43 +1200

    Add zoom and pan functions to map

--- src/pages/Dashboard/SimpleMap.js
+++ src/pages/Dashboard/SimpleMap.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { changeCountry, setTooltip } from '../../reducers/dataReducer/dataActions';
 
-import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from "react-simple-maps";
 import ReactTooltip from 'react-tooltip';
 import NumberFormat from 'react-number-format';
 
@@ -29,50 +29,70 @@ class SimpleMap extends Component {
       const countriesOnMap = countries.filter(a => a.x !== "");
       return (
         <Fragment>
-          <ComposableMap data-tip="" projectionConfig={{ scale: 170 }} style={{ width: "100%", backgroundColor: "#FFFFFF" }}>
-            <Geographies geography={worldGeoData} fill="#DDDDDD" stroke="#FFFFFF" strokeWidth="1">
-              {({ geographies }) =>
-                geographies.map(geo => (
-                  <Geography key={geo.rsmKey} geography={geo} />
-                ))
-              }
-            </Geographies>
-            {countriesOnMap.map((d,i) => (
-              <Marker key={d.country} coordinates={[d.x,d.y]}>
-                <circle
-                  r={Math.log(d.cumConfirmed)}
-                  fill="#CC3D00"
-                  fillOpacity={Math.log(d.cumConfirmed)*0.05}
-                  stroke="#CC3D00"
-                  strokeWidth="1"
-                  onClick={(e) => {
-                    this.props.changeCountry(d.country);
-                  }}
-                  onMouseEnter={() => {
-                    this.props.setTooltip([d.countryName, d.cumConfirmed]);
-                  }}
-                  onMouseLeave={() => {
-                    this.props.setTooltip(['','']);
-                  }}
-                  style={{
-                    default: {
-                      fill: "#D6D6DA",
-                      outline: "none"
-                    },
-                    hover: {
-                      fill: "#F53",
-                      outline: "none"
-                    },
-                    pressed: {
-                      fill: "#E42",
-                      outline: "none"
-                    }
-                  }}
-                />
-              </Marker>
-            ))}
+          <ComposableMap
+            data-tip=''
+            projectionConfig={{ scale: 170 }}
+            style={{ width: "100%", backgroundColor: "#FFFFFF" }}>
+            <ZoomableGroup zoom={1}>
+              <Geographies
+                geography={worldGeoData}
+                fill='#DDDDDD'
+                stroke='#FFFFFF'
+                strokeWidth='1'>
+                {({ geographies }) =>
+                  geographies.map((geo) => (
+                    <Geography key={geo.rsmKey} geography={geo} />
+                  ))
+                }
+              </Geographies>
+              {countriesOnMap.map((d, i) => (
+                <Marker key={d.country} coordinates={[d.x, d.y]}>
+                  <circle
+                    r={Math.log(d.cumConfirmed)}
+                    fill='#CC3D00'
+                    fillOpacity={Math.log(d.cumConfirmed) * 0.05}
+                    stroke='#CC3D00'
+                    strokeWidth='1'
+                    onClick={(e) => {
+                      this.props.changeCountry(d.country);
+                    }}
+                    onMouseEnter={() => {
+                      this.props.setTooltip([d.countryName, d.cumConfirmed]);
+                    }}
+                    onMouseLeave={() => {
+                      this.props.setTooltip(["", ""]);
+                    }}
+                    style={{
+                      default: {
+                        fill: "#D6D6DA",
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: "#F53",
+                        outline: "none",
+                      },
+                      pressed: {
+                        fill: "#E42",
+                        outline: "none",
+                      },
+                    }}
+                  />
+                </Marker>
+              ))}
+            </ZoomableGroup>
           </ComposableMap>
-          { tooltip[0] !== '' && tooltip[0] !== undefined && tooltip[0] !== null && <ReactTooltip>{tooltip[0]} – <NumberFormat value={tooltip[1]} displayType={'text'} thousandSeparator={true} /></ReactTooltip>}
+          {tooltip[0] !== "" &&
+            tooltip[0] !== undefined &&
+            tooltip[0] !== null && (
+              <ReactTooltip>
+                {tooltip[0]} –{" "}
+                <NumberFormat
+                  value={tooltip[1]}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                />
+              </ReactTooltip>
+            )}
         </Fragment>
       );
     } else {
